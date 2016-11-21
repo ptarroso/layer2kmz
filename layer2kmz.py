e49ab6c468b10e7e21c4759bc73bc578a51d823e
Pedro Tarroso <ptarroso@cibio.up.pt>
2016-11-21 10:51:59 +0000

    File header update

--- layer2kmz.py
+++ layer2kmz.py
@@ -3,7 +3,7 @@
 /***************************************************************************
  layer2kmz
                                  A QGIS plugin
- A quick & dirty plugin to build a kmz from a layer of spatial points
+ Build a kmz from a layer of spatial points, lines or polygons
                               -------------------
         begin                : 2016-11-08
         git sha              : $Format:%H$
@@ -15,8 +15,7 @@
  *                                                                         *
  *   This program is free software; you can redistribute it and/or modify  *
  *   it under the terms of the GNU General Public License as published by  *
- *   the Free Software Foundation; either version 2 of the License, or     *
- *   (at your option) any later version.                                   *
+ *   the Free Software Foundation; version 2 of the License.               *
  *                                                                         *
  ***************************************************************************/
 """
--- layer2kmz_dialog.py
+++ layer2kmz_dialog.py
@@ -1,10 +1,10 @@
 # -*- coding: utf-8 -*-
 """
 /***************************************************************************
- layer2kmzDialog
+ layer2kmz
                                  A QGIS plugin
- A quick & dirty plugin to build a kmz from a layer of spatial points
-                             -------------------
+ Build a kmz from a layer of spatial points, lines or polygons
+                              -------------------
         begin                : 2016-11-08
         git sha              : $Format:%H$
         copyright            : (C) 2016 by Pedro Tarroso
@@ -15,12 +15,10 @@
  *                                                                         *
  *   This program is free software; you can redistribute it and/or modify  *
  *   it under the terms of the GNU General Public License as published by  *
- *   the Free Software Foundation; either version 2 of the License, or     *
- *   (at your option) any later version.                                   *
+ *   the Free Software Foundation; version 2 of the License.               *
  *                                                                         *
  ***************************************************************************/
 """
-
 import os
 from PyQt4 import QtGui, uic, QtCore
 
